cef134e7ed4f937180ddd58d2af8f4b28ecd247c
jdamps <joanna.damps@gmail.com>
2019-11-05 19:24:14 +0100

    system logowania + usersession + employsession+

--- header.php
+++ header.php
@@ -36,6 +36,14 @@
 						<button type="submit" name="logout-submit">Wyloguj</button>
 					</form>';
 					}
+						if (isset($_SESSION['eid'])) {
+						echo $_SESSION['elogin'];
+						echo ' jesteś zalogowana.';
+						echo '<form action="includes/logout.inc.php" method="post">
+						<button type="submit" name="logout-submit">Wyloguj</button>
+					</form>';
+						}
+						
 						else {
 						echo '<form action="includes/login.inc.php" method="post">
 						<input type="text" name="loginmail" placeholder="Login lub E-mail">
