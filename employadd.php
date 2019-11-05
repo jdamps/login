afc350ea6bd4a6b77f1b83a90419865468d879dd
jdamps <joanna.damps@gmail.com>
2019-11-05 22:11:34 +0100

    system logowania + usersession + employ and admin session+++

---
+++ employadd.php
@@ -0,0 +1,102 @@
+<?php
+	require "header.php";
+?>
+
+
+<main>
+<h1>------------------------------------</h1>
+<h1>NOWY PRACOWNIK</h1>
+
+		<?php
+		if (isset($_GET['error'])) {
+			if ($_GET['error'] == "emptyfields") {
+				echo '<p>Wypełnij wszystkie pola oznaczone gwiazdką.</p>';
+			}
+			else if ($_GET['error'] == "invalidemail") {
+				echo '<p>Wprowadzony adres e-mail wydaje się być nieprawidłowy.</p>';
+			}
+			else if ($_GET['error'] == "passwordcheck") {
+				echo '<p>Hasła nie są identyczne.</p>';
+			}
+			else if ($_GET['error'] == "usertaken") {
+				echo '<p>Podany login jest niedostępny.</p>';
+			}
+			else if ($_GET['error'] == "sqlerrorr") {
+				echo '<p>Błąd łączenia z bazą.</p>';
+			}
+
+		
+		}
+		else if (isset($_GET['signup'])) {
+			if ($_GET['signup'] == "success") {
+			echo '<p>Konto zostało zarejestrowane.</p>';
+		}
+		}
+		?>
+
+		<form action="includes/employadd.inc.php" method="post"> 
+			<input type="text" name="login_pracownik" placeholder="Login">*
+			<br />
+			<input type="password" name="haslo_pracownik" placeholder="Hasło">*
+			<br />
+			<input type="password" name="haslo_pracownik-rep" placeholder="Powtórz Hasło">*
+			<br />
+			<input type="text" name="imie_pracownik" placeholder="Imię"> 
+			<br />
+			<input type="text" name="nazwisko_pracownik" placeholder="Nazwisko"> 
+			<br />
+			<input type="text" name="tel_pracownik" placeholder="Telefon"> 
+			<br />
+			<input type="text" name="opis_pracownik" placeholder="Opis"> 
+			<br />
+			<button type="submit" name="employ-submit">Dodaj Pracownika</button>
+		</form>
+	</main>
+
+<h1>------------------------------------</h1>
+<h1>PRACOWNICY</h1>
+
+<?php
+
+require './includes/dbh.inc.php';
+
+
+if ($records=mysqli_query($con,"SELECT * FROM pracownicy"))
+	echo "<table width='600' border='1' cellpadding='1' cellspacing='1'>";
+	echo "<th>ID</th>";
+	echo "<th>Login</th>";
+	echo "<th>Imię</th>";
+	echo "<th>Nazwisko</th>";
+	echo "<th>Telefon</th>";
+	echo "<th>Opis</th>";
+	echo "<th>Edytuj</th>";
+	echo "<th>Usuń</th>";
+	
+	while($pk=mysqli_fetch_assoc($records)){
+	
+	echo "<tr>";
+	
+	echo "<td>".$pk['id_pracownik']."</td>";
+	echo "<td>".$pk['login_pracownik']."</td>";
+	echo "<td>".$pk['imie_pracownik']."</td>";
+	echo "<td>".$pk['nazwisko_pracownik']."</td>";
+	echo "<td>".$pk['tel_pracownik']."</td>";
+	echo "<td>".$pk['opis_pracownik']."</td>";
+	echo "<td><a href=edit.php?id=".$pk['id_pracownik'].">Edytuj</a></td>";
+	echo "<td><a href=delete.php?id=".$pk['id_pracownik'].">Usuń</a></td>";
+
+	echo "</tr>";
+	
+	
+}
+
+
+
+mysqli_close($con);
+
+?>
+
+
+<?php
+	require "footer.php";
+?>
---
+++ includes/tests/signup.inc.php
@@ -0,0 +1,86 @@
+<?php
+
+/*po nacisniecu zarejestruj*/
+if (isset($_POST['signup-submit'])) {
+	
+/*polaczenie z baza*/
+	require './dbh.inc.php';
+	
+	$login = $_POST['login_klient'];
+	$email = $_POST['email_klient'];
+	$haslo1 = $_POST['haslo_klient'];
+	$haslo2 = $_POST['haslo_klient-rep'];
+	$chbox = $_POST['box'];
+	
+/*gdy user nie wypelni wszystkich pol*/
+	if (empty($login) || empty($email) || empty($haslo1) || empty($haslo2)) {
+		header ("Location: ../signup.php?error=emptyfields&uid=".$login."&mail=".$email."&Password");
+		exit();
+	}	
+	
+/*sprawdzanie emaila*/
+	else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		header ("Location: ../signup.php?error=invalidemail&uid=".$login);
+	}
+	
+/*sprawdzanie powtorzonego hasla*/
+	else if ($haslo1 !== $haslo2) {
+		header ("Location: ../signup.php?error=passwordcheck&uid=".$login."&mail=".$email);
+	}
+	
+/*sprawdzanie checkboxa*/
+	else if ( empty ($chbox)) {
+		header ("Location: ../signup.php?error=boxerrorr");
+	}	
+	
+/*sprawdzanie czy taki user juz istnieje*/
+	else {
+		
+		$sql = "SELECT login_klient FROM klienci WHERE login_klient=?";
+		$stmt = mysqli_stmt_init($con);
+		if (!mysqli_stmt_prepare($stmt, $sql)) {
+			header ("Location: ../signup.php?error=sqlerrorr");
+			exit();
+		} 
+		
+		else {
+			mysqli_stmt_bind_param($stmt, "s", $login); /*zwiazane z powyzszym statementem*/
+			mysqli_stmt_execute($stmt); /*execiute statement in db*/
+			mysqli_stmt_store_result($stmt); /* stores data from db and stores under variable stmt */
+			$resultCheck = mysqli_stmt_num_rows($stmt); /*how many rows we get from db as resoult - w tym przypadku jedna*/
+			if ($resultCheck > 0) {
+				header ("Location: ../signup.php?error=usertaken&mail=".$email);
+				exit();
+			}
+			else {
+				$sql = "INSERT INTO klienci (login_klient, email_klient, haslo_klient) VALUES (?, ?, ?) ";
+				$stmt = mysqli_stmt_init($con);
+				if (!mysqli_stmt_prepare($stmt, $sql)) {
+					header ("Location: ../signup.php?error=sqlerror");
+					exit();
+				} 
+				else  {
+					$hashedPwd = password_hash($haslo1, PASSWORD_DEFAULT);
+					mysqli_stmt_bind_param($stmt, "sss", $login, $email, $hashedPwd ); 
+					mysqli_stmt_execute($stmt); 
+					header ("Location: ../signup.php?signup=success");
+					exit();
+				
+	
+				}
+			}
+		
+		}
+		
+	}
+	mysqli_stmt_close($stmt);
+	mysqli_close($con);
+	
+
+}
+else { 
+		header ("Location: ../signup.php");
+		exit();
+
+}
+
