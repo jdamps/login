4e58e1425fdcd2457b0ce7bf43a7f5e958e9a867
jdamps <joanna.damps@gmail.com>
2019-11-05 21:06:16 +0100

    system logowania + usersession + employsession++

---
+++ adminsession.php
@@ -0,0 +1,75 @@
+<?php
+require "header.php";
+?>
+
+<main>
+<?php
+
+if (isset($_SESSION['aid'])) {
+	
+require 'includes/dbh.inc.php';
+echo "<br />";
+echo "to jest sesja admina";
+
+
+$eid = $_SESSION['aid'];
+
+	
+/*
+if ($records=mysqli_query($con,"SELECT * FROM pracownicy WHERE id_pracownik=".$eid));
+
+if ($records=mysqli_query($con,"SELECT * FROM pracownicy WHERE id_pracownik=27"));
+
+	while($pk=mysqli_fetch_assoc($records)){
+	echo "<tr><form method=POST action=includes/userupdate.inc.php>";
+	echo "<tr>"; 
+	echo "<br />";
+	echo "Twoje Dane:";
+	echo "<td><input type=text readonly=readonly name=login_klient value='".$pk['login_pracownik']."'></td>";
+
+	echo "<br />";
+	echo "<br />";
+	echo "Imię:";
+	echo "<br />";
+	echo "<td><input type=text name=imie_klient value='".$pk['imie_klient']."'></td>";
+	echo "<button type=submit name=klient-submit>Dodaj/Zmień</button>";
+	echo "<br />";
+	echo "<br />";
+	echo "Nazwisko:";
+	echo "<br />";
+	echo "<td><input type=text name=nazwisko_klient value='".$pk['nazwisko_klient']."'></td>";
+	echo "<button type=submit name=klient-submit>Dodaj/Zmień</button>";
+	echo "<br />";
+	echo "<br />";
+	echo "E-mail:";
+	echo "<br />";
+	echo "<td><input type=text name=email_klient value='".$pk['email_klient']."'></td>";
+	echo "<button type=submit name=klient-submit>Dodaj/Zmień</button>";
+	echo "<br />";
+	echo "<br />";
+	echo "Telefon:";
+	echo "<br />";
+	echo "<td><input type=text name=tel_klient value='".$pk['tel_klient']."'></td>";
+	echo "<button type=submit name=klient-submit>Dodaj/Zmień</button>";
+	echo "<br />";
+	echo "</tr>";
+	echo "<br />";
+	echo '<a href="./userpwedit.php">Zmień hasło</a>';
+	echo "<br />";
+	echo '<a href="./userdelete.php">Usuń Konto</a>';
+	echo "<br />";
+	echo "<a href=strona>Moje wizyty</a>";
+	echo "<br />";
+	
+	
+	
+}
+ 
+*/
+
+mysqli_close($con);
+
+}
+?>
+
+</main>
