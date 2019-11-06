abe980aa8f302fe5b72127d349b87e478d9cf7ed
jdamps <joanna.damps@gmail.com>
2019-11-06 22:32:07 +0100

    system logowania + admin session z user delete + edit

--- employedit.php
+++ employedit.php
@@ -19,3 +19,55 @@ mysqli_close($con);
 }
 ?>
 
+<?php
+
+require './includes/dbh.inc.php';
+
+
+$sql = "SELECT * FROM pracownicy WHERE id_pracownik='$_GET[id]'";
+
+$records = mysqli_query($con,$sql);
+
+?>
+
+	
+	<?php
+	while($row = mysqli_fetch_assoc($records))
+	{
+		echo "<tr><form method=POST action=includes/employupdate.php>";
+		
+		echo "<br />";
+		echo "<td><input type=text readonly=readonly name=login_pracownik value='".$row['login_pracownik']."'></td>";
+		echo "</br >";
+		echo "Imię:";
+		echo "</br >";
+		echo "<td><input type=text name=imie_pracownik value='".$row['imie_pracownik']."'></td>";
+		echo "</br >";
+		echo "Nazwisko:";
+		echo "</br >";
+		echo "<td><input type=text name=nazwisko_pracownik value='".$row['nazwisko_pracownik']."'></td>";
+		echo "</br >";
+		echo "Telefon:";
+		echo "</br >";
+		echo "<td><input type=text name=tel_pracownik value='".$row['tel_pracownik']."'></td>";
+		echo "</br >";
+		echo "Opis:";
+		echo "</br >";
+		echo "<td><input type=text name=opis_pracownik value='".$row['opis_pracownik']."'></td>";
+		echo "<input type=hidden name=id_pracownik value='".$row['id_pracownik']."'>";
+		echo "</br >";
+		echo "<td><button type=submit name=submit >Dodaj/Zmień</button></th>";
+		echo "<button type=submit formaction=./adminsession.php>Powrót</button>";
+		echo "</form></tr>";
+		
+
+		
+	}
+	
+	
+	
+	?>
+	
+<?php
+	require "footer.php";
+?>
--- includes/employupdate.php
+++ includes/employupdate.php
@@ -1,12 +1,12 @@
 <?php
 
-$con=mysqli_connect("localhost","jdamps","admin123","recepcja1");
+require './dbh.inc.php';
 
 $sql = "UPDATE pracownicy SET login_pracownik='$_POST[login_pracownik]', imie_pracownik='$_POST[imie_pracownik]', nazwisko_pracownik='$_POST[nazwisko_pracownik]', tel_pracownik='$_POST[tel_pracownik]', opis_pracownik='$_POST[opis_pracownik]' WHERE id_pracownik='$_POST[id_pracownik]'";
 
 
 	if (mysqli_query($con, $sql)) 
-		header("refresh:1; url=adminsession.php");
+		header("refresh:1; url=../adminsession.php");
 
 	else 
 		echo "Nie udało się edytować danych pracownika";
